4b6c78146d40a110890e5e87b1ee190cc41994ed
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2022-05-14 15:34:33 +0200

    nowa implementacja - rzuty poziome

--- czasteczki.py
+++ czasteczki.py
@@ -28,11 +28,23 @@ class cząsteczka():
         if self.predkosc_x < 0.0000001:
             self.predkosc_x = 0
 
-        self.predkosc_y
+
         self.x += math.cos(self.kąt) * self.predkosc_x
         self.y -= math.sin(self.kąt) * self.predkosc_x - 1.5 * self.g
-        print(self.x)
 
+        print('x = ' + str(self.x))
+        print('y = ' + str(self.y))
+        print('alfa = ' + str(self.kąt))
+
+        self.polozenia_x.append(self.x)
+        self.polozenia_y.append(self.y)
+
+
+        if len(self.polozenia_x) > 1:
+            wektorx = self.polozenia_x[len(self.polozenia_x)-1] - self.polozenia_x[len(self.polozenia_x)-2]
+            wektory = self.polozenia_y[len(self.polozenia_y) - 1] - self.polozenia_y[len(self.polozenia_y) - 2]
+            wektorv = math.sqrt(wektorx**2 + wektory**2)
+            self.kąt = math.asin(wektory/wektorx)
     def odbicie(self):
         if self.y > 800 - self.promien:
             #self.y = 2 * (800 - self.promien - 10) - self.y
