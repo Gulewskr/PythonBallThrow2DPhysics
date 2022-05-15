1ea8e7a16a32cd3b89d2dd382a4b6bd32d99f9d8
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2022-05-15 19:29:36 +0200

    :(

--- czasteczki.py
+++ czasteczki.py
@@ -13,33 +13,37 @@ class cząsteczka():
         self.kat = kat
         self.kat_poczatkowy = kat
         self.predkosc_x = predkosc
-        self.predkosc_y = 0
+        self.predkosc_y = predkosc * math.cos(kat)
         self.g = 0.4
         self.polozenia_x = []
         self.polozenia_y = []
         self.czas = 0
-        self.opor = 0.1
+        self.opor = 2;
+        self.przyspieszenie = 0
 
     def wyswietl(self):
         pygame.draw.circle(okno, self.kolor, (self.x, self.y), self.promien, 0)
 
     def ruch(self):
         self.czas += 0.1
+        self.przyspieszenie = self.g * self.czas
 
         '''if self.predkosc_x > 0:
             self.predkosc_x -= self.opor
         if self.predkosc_x < 0.0000001:
             self.predkosc_x = 0**'''
 
-        self.x += math.cos(self.kat_poczatkowy) * self.predkosc_x
-
         if self.kat >= 0:
             #self.y -= math.sin(self.kąt) * self.predkosc_x
-            self.y += self.g*(self.czas**2)
+            #self.y += self.g*(self.czas**2)
+            self.przyspieszenie = self.przyspieszenie
 
         if self.kat < 0:
-            self.y -= self.g*(self.czas**2)
+            #self.y -= self.g*self.czas - self.g*(self.czas**2)
+            self.przyspieszenie = - self.przyspieszenie
 
+        self.x += math.cos(self.kat_poczatkowy) * self.predkosc_x
+        self.y += self.predkosc_y*self.czas + 0.5 * self.przyspieszenie * self.czas**2
 
         self.polozenia_x.append(self.x)
         self.polozenia_y.append(self.y)
@@ -50,6 +54,8 @@ class cząsteczka():
             wektorv = math.sqrt(wektorx**2 + wektory**2)
             if wektorx != 0:
                 self.kat = math.atan(wektory / wektorx)
+
+        self.predkosc_y = self.predkosc_y * math.cos(self.kat)
 
     def odbicie(self):
         if self.y > 800 - self.promien:
