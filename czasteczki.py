204313fc01ee70a5123125888e84f70a04752afc
Bartlomiej Filipiuk <01169277@pw.edu.pl>
2022-05-18 11:23:04 +0200

    dzialajacy ruch w osi x i y, do naprawy odbicia (mierzenie aktualengo kąta ruchu sfery)

--- czasteczki.py
+++ czasteczki.py
@@ -4,6 +4,7 @@ import numpy as np
 
 okno = pygame.display.set_mode((1000, 800))
 
+
 class cząsteczka():
     def __init__(self, x, y, promien, predkosc, kat):
         self.promien = promien
@@ -22,12 +23,13 @@ class cząsteczka():
         self.prev_wznoszenia = 0
         self.czas_opadania = 0
         self.prev_opadania = 0
+        self.c = 0
 
     def wyswietl(self):
         pygame.draw.circle(okno, self.kolor, (self.x, self.y), self.promien, 0)
 
     def ruch(self):
-        self.tick += 1
+        self.c += 1
         self.x += math.cos(self.kat) * self.predkosc
 
         if 0 < self.kat:
@@ -41,25 +43,33 @@ class cząsteczka():
         self.polozenia_x.append(self.x)
         self.polozenia_y.append(self.y)
 
-        if len(self.polozenia_x) > 1:
-            wektorx = self.polozenia_x[len(self.polozenia_x)-1] - self.polozenia_x[len(self.polozenia_x)-2]
-            wektory = self.polozenia_y[len(self.polozenia_y) - 1] - self.polozenia_y[len(self.polozenia_y) - 2]
-            wektorv = math.sqrt(wektorx**2 + wektory**2)
+        if len(self.polozenia_y) > 4:
+            if self.polozenia_y[len(self.polozenia_y) - 3] < self.polozenia_y[len(self.polozenia_y) - 4] and self.polozenia_y[len(self.polozenia_y) - 1] > self.polozenia_y[len(self.polozenia_y) - 2]:
+                self.kat = - self.kat
 
-            if wektorx != 0:
-                self.kat = math.atan(wektory/ wektorx)
-
-            print(self.kat)
-
+        # if len(self.polozenia_x) > 1:
+        #     wektorx = self.polozenia_x[len(self.polozenia_x) - 1] - self.polozenia_x[len(self.polozenia_x) - 2]
+        #     wektory = self.polozenia_y[len(self.polozenia_y) - 1] - self.polozenia_y[len(self.polozenia_y) - 2]
+        #     wektorv = math.sqrt(wektorx ** 2 + wektory ** 2)
+        #
+        #     if wektorx != 0:
+        #         self.kat = math.atan(wektory / wektorx)
+        #
+        #     print(self.kat)
+        #
         self.katy.append(self.kat)
+
+        print(str(self.czas_opadania) + " prev: " + str(self.prev_opadania))
+
         if 800 - self.promien - 30 < self.y < 800 - self.promien - 10:
             self.prev_opadania = self.czas_opadania
 
         if self.katy[len(self.katy) - 1] < self.katy[len(self.katy) - 2]:
-            self.czas_wznoszenia = 0
-
-        if self.katy[len(self.katy) - 1] > self.katy[len(self.katy) - 2]:
+            #self.czas_wznoszenia = 0
             self.czas_opadania = 0
+        if self.katy[len(self.katy) - 1] > self.katy[len(self.katy) - 2]:
+            #self.czas_opadania = 0
+            self.czas_wznoszenia = 0
 
         #print("t_wznoszenia: " + str(self.czas_wznoszenia) + "  t_opadania: " + str(self.czas_opadania))
 
